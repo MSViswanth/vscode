384c16b045183b8fc865dfa83f65a5ad331a6817
Sai Viswanth Mullapudi <viswanth.mullapudi28@gmail.com>
2024-03-23 15:48:21 -0400

    Open new webview beside the current panel

--- extensions/markdown-language-features/src/commands/openImageInNewEditor.ts
+++ extensions/markdown-language-features/src/commands/openImageInNewEditor.ts
@@ -12,10 +12,41 @@ export class OpenImageInNewEditorCommand implements Command {
 
 	public constructor(
 		private readonly _webviewManager: MarkdownPreviewManager,
+		private _currentPanel: vscode.WebviewPanel | undefined = undefined
 	) { }
 
 	public execute(args: { id: string; resource: string }) {
+		const columnToShowIn = vscode.window.activeTextEditor
+			? vscode.window.activeTextEditor.viewColumn
+			: undefined;
 		const source = vscode.Uri.parse(args.resource);
+		if (this._currentPanel) {
+			// If we already have a panel, show it in the target column
+			this._currentPanel.reveal(columnToShowIn);
+		} else {
+			this._currentPanel = vscode.window.createWebviewPanel(
+				'opemImage', // Identifies the type of the webview. Used internally
+				'Image', // Title of the panel displayed to the user
+				vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+				{}
+			);
+		}
+		// console.log(source);
 		this._webviewManager.findPreview(source)?.openImageInNewEditor(args.id);
+		this._currentPanel.webview.html = getWebviewContent(1);
 	}
 }
+function getWebviewContent(imgId: number): string {
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Cat Coding</title>
+	</head>
+	<body>
+		<img src="${[imgId]}" width="300" />
+	</body>
+	</html>`;
+}
+
